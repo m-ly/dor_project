661088e974900abb7b6606e138eb47bc809ea78d
Matt Lyell <mlyell21@Gmail.com>
2023-02-02 06:05:40 -0800

    completed basic form skeleton

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source "https://rubygems.org"
 gem "sinatra", "~>1.4.7"  # This includes ERB
 gem "sinatra-contrib"
 
+
 # ERB helper -- 
 gem "erubis"
 # Web Server dependency
--- dor.rb
+++ dor.rb
@@ -1,6 +1,8 @@
 require "sinatra"
+require "sinatra/content_for"
 require "sinatra/reloader" if development?
 require "tilt/erubis"
+require "date"
 
 
 configure do 
@@ -15,13 +17,17 @@ before do
   session[:trainees] ||=[]
 end
 
-def load_users
-  credentials_path = if ENV["RACK_ENV"] == "test"
-    File.expand_path("../test/users.yml", __FILE__)
-  else
-    File.expand_path("../users.yml", __FILE__)
-  end
-  YAML.load_file(credentials_path)
+# convienence functions
+def load_form(index)
+  form = session[:forms][index] if index <= session[:forms].size
+  return form if form
+
+  session[:error] = "That form does not exist"
+  redirect  "/forms"
+end 
+
+def save_blank_form(form)
+
 end 
 
 get "/" do 
@@ -33,32 +39,34 @@ get "/forms/create" do
 end 
 
 post "/forms/create" do
-  @form_id = 0 
- 
-  session[:forms] << { document_name: params[:document_name], criteria: [], form_id: @form_id +=1 } 
-  redirect "/forms/#{@form_id}"
+  form_id = session[:forms].size
+
+  session[:forms] << { document_name: params[:document_name], criteria: [], form_id: form_id} 
+  save_blank_form
+  redirect "/forms/#{form_id}/edit"
 end 
+
+post "/forms/:form_id/delete/:item_id" do
+  form_id = params[:form_id].to_i
+  item_id = params[:item_id].to_i
+
+  current_form = session[:forms][form_id]
+  current_form[:criteria].delete_at(item_id)
+
+  redirect "/forms/#{form_id}/edit"
+end 
+
+post "/forms/:form_id/delete" do
+  form_id = params[:form_id].to_i
+  session[:forms].delete_at(form_id)
+
+  redirect "/forms/#{form_id}/edit"
+end
 
 get "/forms" do
   @forms = session[:forms]
   erb :forms, layout: :layout
 end 
-
-
-# Loading a specific form 
-def load_form(index)
-  form = session[:forms][index] if index <= session[:forms].size
-  return form if form
-
-  session[:error] = "That form does not exist"
-  redirect  "/forms"
-end 
-
-def load_segment(criteria)
-  if params[:criteria]
-  end
-    
-end
 
 get "/forms/:form_id" do
   @form_id = params[:form_id].to_i
@@ -71,15 +79,54 @@ get "/forms/:form_id" do
   erb :view_form, layout: :layout
 end
 
-post "/forms/:form_id" do
+get "/forms/:form_id/edit" do
+  @form_id = params[:form_id].to_i
+  form = load_form(@form_id)
+  
+  @form_name = form[:document_name] 
+
+  @criteria = form[:criteria]
+  
+  erb :form_edit,layout: :layout 
+end
+
+
+post "/forms/:form_id/criteria" do
   @form_id = params[:form_id].to_i
   form = load_form(@form_id)
   
   @criteria = form[:criteria]
- 
-  @criteria << params[:criteria]
+
+  @criteria << { text: params[:text], type: "graded_criteria" }
+
+  redirect "/forms/#{@form_id}/edit"
+end
+
+post '/forms/0/save' do
+  session[:params]
   
-  redirect "/forms/#{@form_id}"
+end
+
+post "/forms/:form_id/section-header" do
+  @form_id = params[:form_id].to_i
+  form = load_form(@form_id)
+  
+  @criteria = form[:criteria]
+
+  @criteria << { text: params[:text], type: "section_header"}
+
+  redirect "/forms/#{@form_id}/edit"
+end
+
+post "/forms/:form_id/text-box" do
+  @form_id = params[:form_id].to_i
+  form = load_form(@form_id)
+  
+  @criteria = form[:criteria]
+
+  @criteria << { text: params[:text], type: "text_box" }
+
+  redirect "/forms/#{@form_id}/edit"
 end
 
 get "/trainees" do
@@ -101,8 +148,6 @@ post "/trainers/new" do
   session[:trainers] << params[:add_trainer]
   redirect "/trainers"
 end 
-
-
 
 get "/trainees/:trainee" do
   @trainee = params[:trainee]
